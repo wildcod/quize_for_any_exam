21b0b69836c5c72f61bf6468c485c930238771dd
wildcod <kanojias494@gmail.com>
2019-09-08 00:16:54 +0530

    changes

--- javascript.js
+++ javascript.js
@@ -63,6 +63,43 @@ $(function(){
      
     }))
 
+    $('#skip').click(function(){
+        if(curQues+1 <= dataLength){
+            $(`#dot-${curQues}`).css('background', 'red');
+            curQues++;
+            renderQuestion();
+         }
+    })
+
+    var countDownDate = new Date(2019,09,8,12,20,0,0).getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  $("#timer").empty();
+  $("#timer").append(minutes + "m " + seconds + "s ");
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    $("#timer").empty();
+    $("#timer").append("EXPIRED");
+  }
+}, 1000);
+
+
+
 })
 
 const data = {
